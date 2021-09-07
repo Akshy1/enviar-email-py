0f7473eb838738d97f6d6825e8dcc7267fc0a7eb
Akshy1 <88923353+Akshy1@users.noreply.github.com>
2021-09-07 05:14:49 -0300

    Index.py

--- index.py
+++ index.py
@@ -1,12 +1,28 @@
-<tablet>
-	<tr> Nome </tr>
-	<tr> Idade </tr>
-	<tr> Telefone </tr>
-	
-	<tr>
-	<tr> Breno </tr>
-	<tr> 18 </tr>
-	<tr> 12312312933 </tr>
-	</tr>
+from flask import Flask, jsonify, render_template, request
+from gevent.pywsgi import  WSGIServer
+import requests
 
-</tablet>
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+
+def raiz():
+	
+	return render_template('/email.html', email='email')
+	
+@app.route('/dados', methods=['GET', 'POST'])
+
+def dados():
+	
+	email = request.form['email']
+	with open ('dados.txt', 'a') as arquivo:
+		arquivo.write(str(email) + '\n')
+	
+	return  '<h1> %s <h1>' %(email)
+	
+	
+#app.run()
+server = WSGIServer(('localhost', 5000), app)
+server.serve_forever()
+
